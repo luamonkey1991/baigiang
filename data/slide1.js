c58c66aaae14f4bca527f599f759c0ac8034cd42
luamonkey1991 <54792458+luamonkey1991@users.noreply.github.com>
2025-11-25 13:46:34 +0700

    OK

---
+++ data/slide1.js
@@ -0,0 +1,15 @@
+(function(){var loadHandler=window['sl_{58A267F2-68A7-439C-AEBE-72ACC36326F1}'];loadHandler&&loadHandler(0, '<div id="spr0_277bbff"><div id="spr1_277bbff" class="kern slide"><img id="img3_277bbff" src="data/img3.png" width="720px" height="540px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_277bbff" class="kern slide"><div id="spr3_277bbff" style="left:85px;top:9px;"><img id="img0_277bbff" src="data/img0.png" width="563" height="48" alt="LIEN CHIEU DISTRICT PEOPLE’S COMMITTEES\
+E –LEARNING LESSON PLAN COMPETITION\
+"/></div><div id="spr4_277bbff" style="left:8px;top:113px;"><img id="img1_277bbff" src="data/img1.png" width="699" height="132" alt="THEME: ME AND THE WORLD AROUND\
+UNIT 17: WHAT WOULD YOU LIKE TO EAT?\
+LESSON 1 (PART 1, 2, 3)\
+BASIC ENGLISH 5\
+\
+"/></div><div id="spr5_277bbff" style="left:54px;top:292px;"><img id="img2_277bbff" src="data/img2.png" width="611" height="248" alt=" AUTHOR: HUYNH PHAM TUYET LINH\
+      NGO SI LIEN PRIMARY SCHOOL,\
+        LIEN CHIEU DISTRICT, DANANG CITY\
+            GMAIL: TUYETLINH121091@GMAIL.COM –  PHONE: 0934736361\
+\
+\
+\
+       DECEMBER, 2022"/></div></div></div>', '{"s":[]}');})();
